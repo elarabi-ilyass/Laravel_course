555e44cdaa488e672c4c17859f80e3ca66fdb642
ilyass <ilyassselarabi1999@gmail.com>
2024-12-25 09:04:22 +0100

    Execute seed and use factory to regenerate data with faker

---
+++ database/factories/ProfileFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Profile>
+ */
+class ProfileFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => Hash::make('password'), // Génération d'un mot de passe haché
+            'Description' => $this->faker->text(100), // Limitation de la longueur de la description
+        ];
+    }
+}
--- database/seeders/profileSeeder.php
+++ database/seeders/profileSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Profile;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -14,11 +15,12 @@ class ProfileSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('profiles')->insert([
-            'name' => Str::random(30), // Correspond à la taille définie dans la migration
-            'email' => Str::random(10) . '@gmail.com', // Évitez de dépasser 255 caractères
-            'password' => Hash::make('password'), // Hash compatible avec 255 caractères
-            'Description' => Str::random(200), // Correspond à la colonne `text`
-        ]);
+        Profile::factory(20)->create();
+        // DB::table('profiles')->insert([
+        //     'name' => Str::random(30), // Correspond à la taille définie dans la migration
+        //     'email' => Str::random(10) . '@gmail.com', // Évitez de dépasser 255 caractères
+        //     'password' => Hash::make('password'), // Hash compatible avec 255 caractères
+        //     'Description' => Str::random(200), // Correspond à la colonne `text`
+        // ]);
     }
 }
