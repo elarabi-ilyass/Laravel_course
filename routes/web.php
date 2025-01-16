0b2f462270d6255e138af5e6889b542e8ff7041a
ilyass <ilyassselarabi1999@gmail.com>
2025-01-16 15:04:53 +0100

    Add publications table with model and his ressource controle

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\homeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PublicationController;
 
 
 /*
@@ -33,38 +32,9 @@ Route::post('/LoginForm', [LoginController::class, 'Login'])->name('Login');
 Route::get('/Logout', [LoginController::class, 'Logout'])->name('Login.Logout');
 Route::get('/About',[ProfileController::class,'indexTest']);
 
+Route::get('publications',[PublicationController::class]);
 
-//Header
-// Route::view('/test','Test.test');
-// Route::get('/test', function (Request $request) {
-//     $response=new Response(['data'=>'Ok']);
-//     dd($request->header('Connection'));
-//     return $response->withHeaders([
-//         'Access-Control-Allow-Origin '=> '*',
-//         'Access-Control-Allow-Methods'=> 'GET, POST, PUT, DELETE, OPTIONS',
-//         'Access-Control-Allow-Headers'=> 'Content-Type, Accept, Authorization, X-Requested-With',
-//     ]);
 
-// })->name('Test');
-
-Route::view('/test','Test.test');
-Route::get('/test', function (Request $request) {
-//    dd($request->url(),$request->fullUrl());
-//    dd($request->path(),$request->route());
-//    dd($request->duplicate(),$request->getLanguages());
-// dd($request->isFromTrustedProxy(),$request->is('test'));
-dd($request->isMethod('GET'),$request->capture());
-
-})->name('Test')->middleware('auth');
-
-// Header + request
-  //Content-type:text/html;
-  //Accept:application/json
-  //Accept-Language:fr-FR
-  //Accept-Encoding:gzip, deflate
-  //User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64)
-  //Accept:application/json
-// server -> browser
 
 
 
